e0f383a8f52f51343721efd2d8be3f1ceb9cc8ef
Rafael S.G <88804353+RafaelGou-py@users.noreply.github.com>
2024-04-09 18:44:52 -0300

    Add files via upload

---
+++ projeto1SD/src/ServerArquivos.java
@@ -0,0 +1,81 @@
+import java.io.*;
+import java.net.*;
+import java.util.concurrent.*;
+
+public class ServerArquivos{
+
+    public static void main(String[] args) throws IOException {
+        ExecutorService executor = Executors.newFixedThreadPool(100);
+        ServerSocket servidorSocket = new ServerSocket(1212);
+        System.out.println("Servidor ligado na porta 1212");
+
+        while (true){
+            Socket clienteSocket = servidorSocket.accept();
+            executor.submit(new ClientHandler(clienteSocket));
+        }
+    }
+
+    static class ClientHandler implements Runnable{
+        private final Socket clienteSocket;
+
+        public ClientHandler(Socket socket){
+            this.clienteSocket = socket;
+        }
+
+        public void run(){
+            try{
+                DataInputStream dis = new DataInputStream(clienteSocket.getInputStream());
+                DataOutputStream dos = new DataOutputStream(clienteSocket.getOutputStream());
+
+                String fileName = dis.readUTF();
+                String operation = dis.readUTF();
+
+                switch (operation){
+                    case "UPLOAD":
+                        FileOutputStream fos = new FileOutputStream(fileName);
+                        byte[] buffer = new byte[4096];
+
+                        int filesize = 15000; //Tamanho do arquivo
+                        int read = 0;
+                        int totalRead = 0;
+                        int remaining = filesize;
+                        while((read = dis.read(buffer, 0, Math.min(buffer.length, remaining))) > 0){
+                            totalRead += read;
+                            remaining -= read;
+                            fos.write(buffer, 0, read);
+                        }
+
+                        fos.close();
+                        dos.writeUTF("Arquivo: " + fileName + " RECEBIDO corretamente.");
+                        break;
+
+                    case "DOWNLOAD":
+                        FileInputStream fis = new FileInputStream(fileName);
+                        buffer = new byte[4096];
+
+                        while ((read = fis.read(buffer)) != -1){
+                            dos.write(buffer, 0, read);
+                        }
+
+                        fis.close();
+                        dos.writeUTF("Arquivo: " + fileName + " ENVIADO corretamente.");
+                        break;
+
+                    case "DELETE":
+                        File file = new File(fileName);
+                        if(file.delete()){
+                            dos.writeUTF("Arquivo: " + fileName + " EXCLUIDO com SUCESSO!");
+                        }else{
+                            dos.writeUTF("Arquivo: " + fileName + " não encontrado.");
+                        }
+                        break;
+                }
+                dis.close();
+                dos.close();
+                clienteSocket.close();
+            }catch (IOException e){
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
